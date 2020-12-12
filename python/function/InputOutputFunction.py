4661827a1fc01f6e2b2729ed5d30660c47789614
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-12-12 16:43:20 +0900

    입 출력 함수

---
+++ python/function/InputOutputFunction.py
@@ -0,0 +1,20 @@
+# 입 출력 함수 예제
+
+temp = input("입력 함수 : ")
+
+print(temp)
+
+
+a = "gil"
+b = "log"
+c = "python"
+d = "basic"
+
+print(a+b+c+d)
+print("gil""log""python""basic")
+
+print(a, b, c, d)
+print("gil log python basic")
+
+for i in range(10):
+    print(i, end=' ')
