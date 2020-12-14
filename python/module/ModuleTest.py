9352340767fc3a7ac9bcd0f3b2c0441b18ce1fd7
swgil007 <swgil007@naver.com>
2020-12-14 10:45:57 +0900

    모듈 테스트

---
+++ python/module/ModuleTest.py
@@ -0,0 +1,13 @@
+# 모듈 테스트
+
+import gillog
+
+gil = gillog.Gil
+
+gilMethod = gillog.gil()
+
+gil.gillog()
+
+print(gil.name)
+
+gilMethod
---
+++ python/module/gillog.py
@@ -0,0 +1,9 @@
+# 모듈
+
+class Gil:
+    name = "gillog"
+    def gillog():
+        print("gillog 작성")
+
+def gil():
+    print("gil")
