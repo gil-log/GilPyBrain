cc28f3b589b3fbe453682d3ca7a74305dbcf820f
swgil007 <swgil007@naver.com>
2020-12-15 14:08:10 +0900

    에러 처리

---
+++ python/exception/exceptionExample.py
@@ -0,0 +1,15 @@
+# 예외처리
+
+try:
+    4 / 0
+except ZeroDivisionError as e:
+    print(e)
+
+try:
+    a = [1,2]
+    print(a[3])
+    4/0
+except ZeroDivisionError:
+    print("0으로 나눌 수 없습니다.")
+except IndexError:
+    print("인덱싱 할 수 없습니다.")
