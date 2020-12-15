bac57189ed6649ce2dba639cd25785ccf269861a
swgil007 <swgil007@naver.com>
2020-12-15 16:13:20 +0900

    내장 함수

--- python/innerfunction/innerfunction.py
+++ python/innerfunction/innerfunction.py
@@ -9,3 +9,18 @@ for i, name in enumerate(['body', 'foo', 'bar']):
     print(i, name)
 
 print(eval("'gil'+'log'"))
+
+print(hex(234))
+
+a = 3
+b = a
+
+print(id(a))
+print(id(3))
+print(id(b))
+print(id(5))
+
+print(oct(32))
+
+print(ord('a'))
+print(round(5.678, 2))
