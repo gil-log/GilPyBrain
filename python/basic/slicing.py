d228e3eb5535df43fa79aadbdd79448a814c15af
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-12-19 11:31:33 +0900

    slicing 예제

--- python/basic/slicing.py
+++ python/basic/slicing.py
@@ -7,3 +7,19 @@ print(obj[-2])
 
 #####
 
+print(obj[3:])
+print(obj[-3:])
+
+
+print(obj[:3])
+print(obj[:-3])
+
+print(obj[2:4])
+print(obj[-4:-2])
+
+print(obj[::2])
+print(obj[1:5:2])
+
+
+print(obj[::-1])
+print(obj[4::-2])
