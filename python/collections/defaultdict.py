2fc8462252ce66c5a3ecbc13432ede452432ad3c
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-12-26 20:02:39 +0900

    defaultdict module 예제

---
+++ python/collections/defaultdict.py
@@ -0,0 +1,18 @@
+# Collections defaultdict module 예제
+
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('IBM', 50, 91.1),
+    ('CAT', 150, 83.44),
+    ('IBM', 100, 45.23),
+    ('GOOG', 75, 572.45),
+    ('AA', 50, 23.15)
+]
+
+from collections import defaultdict
+holdings = defaultdict(list)
+for name, shares, price in portfolio:
+    holdings[name].append((shares, price))
+
+print(holdings['IBM'])
+# [ (50, 91.1), (100, 45.23) ]
