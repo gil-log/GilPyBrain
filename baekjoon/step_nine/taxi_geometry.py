4ce5676045737ea5265bbb38e232ab3370be846e
swgil007 <48559894+swgil007@users.noreply.github.com>
2020-12-28 05:03:46 +0900

    택시 기하학

--- baekjoon/step_nine/taxi_geometry.py
+++ baekjoon/step_nine/taxi_geometry.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/3053
+
+import math
+
+R = int(input())
+
+
+euclid_circle = math.pi * R ** 2
+
+taxi_circle = (R ** 2) * 2
+
+print('%.6f' %euclid_circle)
+print('%.6f' %taxi_circle)
