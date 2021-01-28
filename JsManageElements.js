73f74e31bc967b8a4d4bcb606826093dc336f059
GilLog <swgil007@naver.com>
2021-01-28 11:34:03 +0900

    Create JsManageElements.js

---
+++ JsManageElements.js
@@ -0,0 +1,87 @@
+
+            //alert(result["recommend_flag"]);
+
+/*            var arrRecommendFood = result["result"];
+
+            var divSlides = document.getElementById("slides");
+
+            //alert(divSlides);
+
+            for(var i = 0; i < arrRecommendFood.length; i++){
+
+                var f_name = arrRecommendFood[i]["f_name"];
+                var f_price = arrRecommendFood[i]["f_price"];
+                var f_img_link = arrRecommendFood[i]["f_img_link"];
+                var r_seq = arrRecommendFood[i]["r_seq"];
+                var f_seq = arrRecommendFood[i]["f_seq"];
+                var f_order_count = arrRecommendFood[i]["f_order_count"];
+
+                if(i>3){
+                    var newDivContent = document.createElement("div");
+                    newDivContent.setAttribute("class", "content_next");
+                    newDivContent.setAttribute("onclick", "clickContent("+r_seq+")");
+                    var newImgTag = document.createElement("img");
+                    newImgTag.src=f_img_link;
+
+                    var newPtagName = document.createElement("p");
+                    newPtagName.setAttribute("class", "menu_name");
+                    newPtagName.innerText = f_name ;
+
+
+
+                    //var newItag = document.createElement("i");
+
+
+
+                    var newPtagCount = document.createElement("p");
+                    newPtagCount.setAttribute("class", "menu_order_count");
+                    newPtagCount.innerText = f_order_count;
+
+                    newDivContent.append(newImgTag);
+                    newDivContent.append(newPtagName);
+
+
+                    //newItag.append(newItag);
+
+
+
+                    newDivContent.append(newPtagCount);
+
+                    divSlides.append(newDivContent);
+                }else{
+                    var newDivContent = document.createElement("div");
+                    newDivContent.setAttribute("class", "content");
+                    newDivContent.setAttribute("onclick", "clickContent("+r_seq+")");
+                    var newImgTag = document.createElement("img");
+                    newImgTag.src=f_img_link;
+
+                    var newPtagName = document.createElement("p");
+                    newPtagName.setAttribute("class", "menu_name");
+                    newPtagName.innerText = f_name;
+
+
+                    //var newItag = document.createElement("i");
+
+                    var newPtagCount = document.createElement("p");
+                    newPtagCount.setAttribute("class", "menu_order_count");
+                    newPtagCount.innerText = f_order_count;
+
+                    newDivContent.append(newImgTag);
+                    newDivContent.append(newPtagName);
+
+                    //newItag.append(newItag);
+
+                    newDivContent.append(newPtagCount);
+
+                    divSlides.append(newDivContent);
+
+                }
+
+
+
+            }*/
+
+
+
+
+
